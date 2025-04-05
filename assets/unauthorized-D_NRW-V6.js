e6a9bb107c98c8d07c3eff7bbe2c87033297bd77
DninoAdnane <da_guettaf@esi.dz>
2025-04-06 00:03:27 +0200

    Updates

---
+++ assets/unauthorized-D_NRW-V6.js
@@ -0,0 +1,1 @@
+import{r as s,j as o}from"./index-CdvkGghb.js";const t=()=>(s.useEffect(()=>(document.body.classList.add("background"),document.body.classList.add("no-footer"),()=>{document.body.classList.remove("background"),document.body.classList.remove("no-footer")}),[]),o.jsxs(o.Fragment,{children:[o.jsx("div",{className:"fixed-background"}),o.jsx("main",{})]}));export{t as default};
