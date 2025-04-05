af9b5b2afd9791eb1d577c437fe17176496a6004
DninoAdnane <da_guettaf@esi.dz>
2025-04-05 23:49:26 +0200

    [feat] change vite config base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/dar-el-mic/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
